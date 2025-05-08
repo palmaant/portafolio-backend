4b3077062dc94be28dd1c053fd5066920d2c9443
palmaant <anthony.p175@outlook.com>
2025-05-08 15:09:51 -0500

    Eliminar todo del repositorio

--- portafolio-backend/proyecto2-autenticacion-usuarios/app/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr # type: ignore
-
-class User(BaseModel):
-    username: str
-    email: EmailStr
-    password: str
-
-class UserInDB(User):
-    hashed_password: str
